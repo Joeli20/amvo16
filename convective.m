d413188fa4ee3b86479038ed2a3dc1f750e05707
Joel Campo Moyà <joel.joel.campo@gmail.com>
2023-09-21 02:40:06 +0200

    functions validated
    To review & improve comments

--- convective.m
+++ convective.m
@@ -12,7 +12,42 @@ function [u_conv,v_conv] = convective(u,v,L)
     % u_conv: Solution of the convective terms of horitzontal velocity
     % v_conv: Solution of the convective terms of vertical velocity
 
+N = size(u,1) - 2;
 
-cu = u + v + L;
+u_conv = zeros(N+2,N+2);
+v_conv = zeros(N+2,N+2);
+
+delta = L/N;
+
+for i = 2:N+1
+    for j = 2:N+1
+        
+        u_e = (u(i+1,j) + u(i,j))/2; % Diapo 12
+        u_w = (u(i-1,j) + u(i,j))/2;
+        u_n = (u(i,j+1) + u(i,j))/2;
+        u_s = (u(i,j-1) + u(i,j))/2;
+        v_e = (v(i+1,j) + v(i,j))/2;
+        v_w = (v(i-1,j) + v(i,j))/2;
+        v_n = (v(i,j+1) + v(i,j))/2;
+        v_s = (v(i,j-1) + v(i,j))/2;
+
+        % A diferència del diffusive, aquí no ens podem petar els delta xq
+        % hi ha sumes pel mig, però si que ho agrupem i fem factor comú
+        % així estalviem capacitat de càlcul.
+
+        Fu_e = (u(i+1,j) + u(i,j))*delta/2; % Diapo 13
+        Fu_w = (u(i-1,j) + u(i,j))*delta/2;
+        Fu_n = (v(i+1,j) + v(i,j))*delta/2;
+        Fu_s = (v(i+1,j-1) + v(i,j-1))*delta/2;
+
+        Fv_e = (u(i,j+1) + u(i,j))*delta/2; % Diapo 13
+        Fv_w = (u(i-1,j) + u(i-1,j+1))*delta/2;
+        Fv_n = (v(i,j+1) + v(i,j))*delta/2;
+        Fv_s = (v(i,j-1) + v(i,j))*delta/2;
+
+        u_conv(i,j) = u_e*Fu_e - u_w*Fu_w + u_n*Fu_n - u_s*Fu_s; %Diapo 11
+        v_conv(i,j) = v_e*Fv_e - v_w*Fv_w + v_n*Fv_n - v_s*Fv_s ;
+    end
+end
 
 end
--- diffusive.m
+++ diffusive.m
@@ -1,4 +1,4 @@
-function [u_conv,v_conv] = diffusive(u,v,L)
+function [u_diff,v_diff] = diffusive(u,v,L)
 % Integral of diffusive term, x and y components. L is the domain size.
 
 % Written by: Joel Campo, Jordi Gallart, Martí Santamaria, 2023
@@ -9,10 +9,35 @@ function [u_conv,v_conv] = diffusive(u,v,L)
     % v: Matrix of the vertical velocity components
     % L: length of a side of the analysed square
 % Outputs:
-    % u_conv: Solution of the diffusive terms of horitzontal velocity
-    % v_conv: Solution of the diffusive terms of vertical velocity
+    % u_diff: Solution of the diffusive terms of horitzontal velocity
+    % v_diff: Solution of the diffusive terms of vertical velocity
 
+N = size(u,1) - 2;
 
-cu = u + v + L;
+u_diff = zeros(N+2,N+2);
+v_diff = zeros(N+2,N+2);
+
+for i = 2:N+1
+    for j = 2:N+1
+        % m'he petat el delta xq en aquest cas al ser quadrat s'anula i
+        % així guanyem temps de càlcul. a la diapo 16 es veu com hauria de
+        % ser, que és el sx(i+1) - sx(i) a sota i després multiplicat pel
+        % sy(j) - sy(j-1), però a efectes pràctics és la mateixa distància
+
+        du_e = u(i+1,j) - u(i,j); % diapo 16
+        du_w = u(i,j) - u(i-1,j);
+        du_n = u(i,j+1) - u(i,j);
+        du_s = u(i,j) - u(i,j-1);
+        
+        dv_e = v(i+1,j) - v(i,j); % diapo 16
+        dv_w = v(i,j) - v(i-1,j);
+        dv_n = v(i,j+1) - v(i,j);
+        dv_s = v(i,j) - v(i,j-1);
+
+        u_diff(i,j) = du_e - du_w + du_n - du_s ; % diapo 16
+        v_diff(i,j) = dv_e - dv_w + dv_n - dv_s ;
+    end 
+end 
+
 
 end
