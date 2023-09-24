2073aac12f6004d65a2b4eae9e71463c85a0c169
MartiSantamaria <marti.santamaria.aldoma@estudiantat.upc.edu>
2023-09-24 18:53:25 +0200

    main Part A (és necessai)

---
+++ PartA_16.m
@@ -0,0 +1,7 @@
+%% MUEA - AMVO
+% ESEIAAT, UPC - MUEA (2023)
+% Joel Campo, Jordi Gallart, Martí Santamaria
+
+% Main de la part A (cada part s'ha d'entregar individualment)
+
+clc ; clear all ; close all ;
