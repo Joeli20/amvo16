6e2456c68cedfbc64f2224f180a50ce2815857b6
MartiSantamaria <marti.santamaria.aldoma@estudiantat.upc.edu>
2023-09-29 19:23:16 +0200

    start laplacian

---
+++ laplacianMatrix.m
@@ -0,0 +1,16 @@
+function A = laplacianMatrix(N)
+% laplacianMatrix 
+
+% Written by: Joel Campo, Jordi Gallart, Martí Santamaria, 2023
+% Group 16. AMVO. MUEA.
+
+% Inputs:
+    % N: 
+% Outputs:
+    % A: 
+
+
+
+
+
+% Diapos 39,42
