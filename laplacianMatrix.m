d6fdb5ac0b076dbbb0560f63dfab984cd9d63e9f
MartiSantamaria <marti.santamaria.aldoma@estudiantat.upc.edu>
2023-09-30 18:18:22 +0200

    laplace fi

--- laplacianMatrix.m
+++ laplacianMatrix.m
@@ -9,11 +9,35 @@ function A = laplacianMatrix(N)
 % Outputs:
     % A: Laplacian matrix
 
-% Diapos 46-47
+% Diapo 46
 
-A = zeros(N^2,N^2);
+A = zeros(N^2,N^2); % Laplacian matrix
+v = 1:N^2;
+a = vector2field(v);
+a = halo_update(a);
 
+for i = 2:N+1
+    for j = 2:N+1
+        e_n = a(i+1,j); % east neighbour
+        w_n = a(i-1,j); % west neighbour
+        n_n = a(i,j+1); % north neighbour
+        s_n = a(i,j-1); % south neighbour
+        A(a(i,j),e_n) = 1;
+        A(a(i,j),w_n) = 1;
+        A(a(i,j),n_n) = 1;
+        A(a(i,j),s_n) = 1;
+        A(a(i,j),a(i,j)) = -4;
+    end
+end
 
+% Comprobem que sigui una matriu singular
+for i = 1:N^2
+    if sum(A(i,:)) ~= 0
+        error('The matrix is not singular')
+    end
+    if sum(A(:,j)) ~= 0
+        error('The matrix is not singular')
+    end
+end
 
-
-
+end
