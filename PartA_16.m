bfa49cc50e1b2e87be148b1aedefcef8a8f93db3
Joel Campo Moyà <joel.joel.campo@gmail.com>
2023-10-01 13:35:03 +0200

    correct fnc; generalizing for; reparing analytic calculation

--- PartA_16.m
+++ PartA_16.m
@@ -8,11 +8,11 @@ clc ; clear all ; close all ;
 
 syms x y
 L = 1;
-f_u = sin(x)*cos(y);
-f_v = cos(x)*sin(y);
-n = [3,9,27,81];
+f_u = cos(2*pi*x)*sin(2*pi*y);
+f_v = -sin(2*pi*x)*cos(2*pi*y);
+n = [8,16,32,64,128];
 
-for i = 1:4
+for i = 1:length(n)
 N = n(i);
 h = L/N;
 
@@ -26,11 +26,8 @@ v = halo_update(v);
 % Analytic (Diapos 7-8)
 [u_conv,v_conv,u_diff,v_diff] = analytic(f_u,f_v,x,y);
 
-[f_u_conv,f_v_conv] = set_velocity_field(N,L,u_conv,v_conv);
-[f_u_diff,f_v_diff] = set_velocity_field(N,L,u_diff,v_diff);
-% solution
-[u_conv_an,v_conv_an] = convective(f_u_conv,f_v_conv,L);
-[u_diff_an,v_diff_an] = diffusive(f_u_diff,f_v_diff);
+[u_conv_an,v_conv_an] = set_velocity_field(N,L,u_conv,v_conv);
+[u_diff_an,v_diff_an] = set_velocity_field(N,L,u_diff,v_diff);
 
 % Numeric solution
 [u_conv_num,v_conv_num] = convective(u,v,L);
