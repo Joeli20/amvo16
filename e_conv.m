17b4b91b1ee5210b9c93750764300a7bd2b354ef
Joel Campo Moyà <joel.joel.campo@gmail.com>
2023-10-14 23:47:59 +0200

    Fer les coses bé

--- e_conv.m
+++ e_conv.m
@@ -1,23 +1,35 @@
-function [Error_Convectiu, min_error_convectiu] = e_conv(u,v,L,N)
-Error_Convectiu = zeros(size(N));
+function [error, min_error] = e_conv(u,v,L,N)
+% e_conv calculates the convective term error
+%
+% Written by: Joel Campo, Jordi Gallart, Martí Santamaria, 2023
+% Group 16. AMVO. MUEA.
+%
+% Inputs:
+%   u: Matrix of the horitzontal components
+%   v: Matrix of the vertical components
+%   L: Length of a side of the analysed square
+%   N: Number of cells of one axis of the square mesh
+% Outputs:
+%   error: Error for the function
+%   min_error: Minimum of all errors
 
-h=L/N;
+error = zeros(size(N));
 
-x=1;
-trigger = 0;
-min_error_convectiu = 5000;
+h = L/N;
+
+x = 1;
+min_error = 5000;
 
 for i = 2:N+1
-    y=1;
+    y = 1;
     for j = 2:N+1
-        Error_Convectiu(x,y)=h/(sqrt(u(i,j)^2+v(i,j)^2));
-        trigger = Error_Convectiu(x,y);
-        y=y+1;
-        if (trigger < min_error_convectiu)
-            min_error_convectiu = trigger;
+        error(x,y) = h/(sqrt(u(i,j)^2 + v(i,j)^2));
+        trigger = error(x,y);
+        y = y + 1;
+        if (trigger < min_error)
+            min_error = trigger;
         end
     end
-    x=x+1;
+    x = x + 1;
 end
-
 end
